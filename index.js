15d4960e101184ece8b6a95564b7f6f3e4de810f
Gtajisan <ffjisan804@gmail.com>
2026-07-21 07:07:03 +0000

    Initialize global GoatBot V2 state and utils at entrypoint index.js

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 'use strict';
 
+// Global GoatBot V2 Ecosystem Setup
+global.utils = require('./utils.js');
+global.GoatBot = global.GoatBot || {};
+global.GoatBot.config = require('./config');
+global.GoatBot.commands = new Map();
+global.GoatBot.aliases = new Map();
+global.GoatBot.onReply = new Map();
+global.GoatBot.onReaction = new Map();
+global.GoatBot.onEvent = new Map();
+global.GoatBot.onChat = new Map();
+global.client = global.client || {};
+
 const InstagramBot = require('./bot/InstagramBot');
 
 const bot = new InstagramBot();
@@ -8,3 +20,4 @@ bot.start().catch(error => {
   console.error('Fatal error:', error.message);
   process.exit(1);
 });
+
